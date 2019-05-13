cadf05884bbe0de776062a2be46ea4df78cf8b64
Ryan McPherson <ryan.mcpherson.182@gmail.com>
2019-05-13 17:59:47 -0500

    ???

---
+++ gulpFile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('style', async function () {
+await gulp.src('style.css')
+.pipe(autoprefixer())
+.pipe(gulp.dest('build'))
+});
+
+                              
+gulp.task('watch', async function () {
+await gulp.watch('style.css', gulp.series('style'));
+return
+});
